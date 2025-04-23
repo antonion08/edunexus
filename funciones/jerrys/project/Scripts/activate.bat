4164b8f97831183f04ede8f84c8ad2b72db5b3c1
Lunathir1 <147435261+Lunathir1@users.noreply.github.com>
2025-04-23 14:02:40 -0400

    entorno jerrys

--- funciones/jerrys/project/Scripts/activate.bat
+++ funciones/jerrys/project/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\informatica\Documents\GitHub\proyectodocker\funciones\jerrys\project
+set VIRTUAL_ENV=C:\Users\hokag\OneDrive\Documentos\GitHub\proyectodocker\funciones\jerrys\project
 
 if not defined PROMPT set PROMPT=$P$G
 
